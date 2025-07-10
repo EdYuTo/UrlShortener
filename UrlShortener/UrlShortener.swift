6907748654eb5f57675b5ea67cf5b9c646fb5f90
EdYuTo <edson.yudi.toma@gmail.com>
2025-07-09 22:23:32 -0300

    Add SceneDelegate to fix warnings

--- UrlShortener/UrlShortener.swift
+++ UrlShortener/UrlShortener.swift
@@ -10,48 +10,4 @@ import NetworkProvider
 import UIKit
 
 @main
-final class UrlShortener: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        let networkProvider = NetworkProvider()
-        let networkDebugDecorator = NetworkDebugDecorator(provider: networkProvider)
-
-        let cacheProvider = CacheProvider(storagePath: cacheDirectory())
-        let cacheDebugDecorator = CacheDebugDecorator(provider: cacheProvider)
-
-        let viewModel = ShortenedListViewModel(
-            cacheProvider: cacheDebugDecorator,
-            networkProvider: networkDebugDecorator
-        )
-        let viewController = ShortenedListViewController(viewModel: viewModel)
-
-        let navigationController = UINavigationController(rootViewController: viewController)
-
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-
-        return true
-    }
-
-    private func cacheDirectory() -> URL {
-        let defaultUrl = URL(fileURLWithPath: "/dev/null")
-        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first,
-              let bundleId = Bundle.main.bundleIdentifier else {
-            return defaultUrl
-        }
-        do {
-            let directory = url
-                .appendingPathComponent(bundleId)
-                .appendingPathComponent("Application")
-            try FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
-            return directory
-        } catch {
-            return defaultUrl
-        }
-    }
-}
+final class UrlShortener: UIResponder, UIApplicationDelegate {}
